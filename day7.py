fb8634562b794e8c0f3719436d3b5ddb6f408041
Maria Zverina <emz@fb.com>
2018-12-09 15:15:16 +0000

    refactored part 1

--- day7.py
+++ day7.py
@@ -9,35 +9,51 @@ with open(__file__[:-2]+"txt", "r") as f:
 # list of tuples (A, B) meaning A preceds B
 deps = [re.match(r'Step ([A-Z]) must be finished before step ([A-Z]) can begin.', line).groups() for line in lines]
 
-items = set(reduce(operator.add, deps))
-print(sorted(items), len(items))
-
-deptree = {k : set() for k in items}
-
-for dep in deps:
-    pre, post = dep
-    deptree[post].add(pre)
-
-print(deps)
-print(sorted(deptree.items()))
 
 class Plan:
-    def __init__(self):
-        pass
+    def __init__(self, deps):
+        items = set(reduce(operator.add, deps))
+        print(sorted(items), len(items))
+        self.deptree = {k: set() for k in items}
+        for dep in deps:
+            pre, post = dep
+            self.add_dep(pre, post)
 
+    def add_dep(self, pre, post):
+        self.deptree[post].add(pre)
+
+    def dep_list(self):
+        return sorted(self.deptree.items())
+
+    def p_queue(self):
+        return sorted(zip(self.deptree.values(), self.deptree.keys()), key=lambda x:(len(x[0]), x))
+
+    def __bool__(self):
+        return bool(self.deptree)
+
+    def process(self, hipri):
+        self.deptree.pop(hipri)
+        for s in self.deptree.values():
+            if hipri in s:
+                s.remove(hipri)
+
+    def next_task(self):
+        return self.p_queue()[0][1]
+
+
+plan = Plan(deps)
+
+# print(deps)
+# print(plan.dep_list())
 
 
 path = ''
-while deptree:
-    q  = sorted(zip(deptree.values(), deptree.keys()), key=lambda x:(len(x[0]), x))
-    hipri = q[0][1]
-    print("q", q)
-    deptree.pop(hipri)
-    for s in deptree.values():
-        if hipri in s:
-            s.remove(hipri)
-    print("popped", hipri, "\n", sorted(deptree.items()))
-    path += hipri
+while plan:
+    # print("q", plan.p_queue())
+    task = plan.next_task()
+    plan.process(task)
+    # print("popped", task, "\n", plan.dep_list())
+    path += task
 
 
 print(path)
